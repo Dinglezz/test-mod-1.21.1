cbb5fc49317f1f70dbf5a2f6589ae90e079b2d6f
Dingle <Stepa202120212021@gmail.com>
2024-11-01 13:34:07 -0600

    Added Chisel Item

--- src/main/java/net/dinglezz/testmod/item/ModItemGroups.java
+++ src/main/java/net/dinglezz/testmod/item/ModItemGroups.java
@@ -19,6 +19,8 @@ public class ModItemGroups {
                         entries.add(ModItems.PINK_GARNET);
                         entries.add(ModItems.RAW_PINK_GARNET);
 
+                        entries.add(ModItems.CHISEL);
+
                         entries.add(ModBlocks.PINK_GARNET_BLOCK);
                         entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
 
--- src/main/java/net/dinglezz/testmod/item/ModItems.java
+++ src/main/java/net/dinglezz/testmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.dinglezz.testmod.item;
 
 import net.dinglezz.testmod.TestMod;
+import net.dinglezz.testmod.item.custom.ChiselItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.dinglezz.testmod.TestMod;
 import net.minecraft.item.Item;
@@ -12,6 +13,8 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name), item);
