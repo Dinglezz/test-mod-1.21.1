6e1e0795d3cf1f4d076e280efd0a0f94178ace43
Dingle <Stepa202120212021@gmail.com>
2024-11-06 23:16:17 -0700

    Horse Armor

--- src/main/java/net/dinglezz/testmod/item/ModItemGroups.java
+++ src/main/java/net/dinglezz/testmod/item/ModItemGroups.java
@@ -34,6 +34,8 @@ public class ModItemGroups {
                         entries.add(ModItems.PINK_GARNET_LEGGINGS);
                         entries.add(ModItems.PINK_GARNET_BOOTS);
 
+                        entries.add(ModItems.PINK_GARNET_HORSE_ARMOR);
+
                         entries.add(ModBlocks.PINK_GARNET_BLOCK);
                         entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
 
